3bffa2385e1f7b1debe1022b9bb0082388326961
Rapeepat Treerattrakul <rapeepat.ptpk@gmail.com>
2025-05-13 17:19:24 +0700

    Implement category operations using Prisma

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,26 +1,41 @@
+const prisma = require('../config/prisma')
+
 exports.create = async (req, res) => {
 	try {
-		res.send("Hello Create Category")
+		const { name } = req.body
+		const category = await prisma.category.create({
+			data: {
+				name: name
+			}
+		})
+		res.send(category)
 	} catch (error) {
 		console.error(error)
-		res.status(500).json({message: "Server Error"})
+		res.status(500).json({ message: "Server Error" })
 	}
 }
 
 exports.list = async (req, res) => {
 	try {
-		res.send("Hello Show Category")
+		const category = await prisma.category.findMany()
+		res.send(category)
 	} catch (error) {
 		console.error(error)
-		res.status(500).json({message: "Server Error"})
+		res.status(500).json({ message: "Server Error" })
 	}
 }
 
 exports.remove = async (req, res) => {
 	try {
-		res.send("Hello Remove Category")
+		const { id } = req.params
+		const category = await prisma.category.delete({
+			where: {
+				id: Number(id)
+			}
+		})
+		res.send(category)
 	} catch (error) {
 		console.error(error)
-		res.status(500).json({message: "Server Error"})
+		res.status(500).json({ message: "Server Error" })
 	}
 }
